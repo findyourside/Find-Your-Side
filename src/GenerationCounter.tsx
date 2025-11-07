6b38b5c3a3a5526d9bc16bfae596b9f4231b46ea
Find Your SIde <hello.findyourside@gmail.com>
2025-11-06 22:27:11 -0500

    Add display for remaining idea sets and playbooks

--- src/GenerationCounter.tsx
+++ src/GenerationCounter.tsx
@@ -14,3 +14,15 @@ export default function GenerationCounter({ ideaSetsRemaining, playbooksRemainin
           <span className="text-gray-700">
             <strong className="text-indigo-600">Idea sets remaining:</strong> {ideaSetsRemaining} of 2
           </span>
+        </div>
+        <div className="text-gray-300">|</div>
+        <div className="flex items-center gap-2">
+          <ClipboardList className="w-4 h-4 text-indigo-600" />
+          <span className="text-gray-700">
+            <strong className="text-indigo-600">Playbooks remaining:</strong> {playbooksRemaining} of 2
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
